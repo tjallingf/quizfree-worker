8f70ae68d73e5068e796c6dc75a7f197af379a49
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-23 20:14:46 +0200

    fix2

--- src/app.js
+++ src/app.js
@@ -15,7 +15,12 @@ const main = async () => {
         // Creating a new browser prevents captcha's
         const browser = await puppeteer.launch({ 
             headless: false,
-            args: ["--no-sandbox"],
+            args: [
+                '--disable-setuid-sandbox',
+                '--no-sandbox',
+                '--single-process',
+                '--no-zygote',
+            ],
             executablePath: process.env.NODE_ENV === 'production'
                 ? process.env.PUPPETEER_EXECUTABLE_PATH
                 : puppeteer.executablePath()
